7c25f7938c97674c6744e337fb8f59a44cce54cc
egan-ombaka <eganombaka97@gmail.com>
2025-01-27 04:40:28 +0300

    A java programm to claculate the volume of a cylinder

---
+++ cylindervolume/cylinderVolume.java
@@ -0,0 +1,42 @@
+package cylindervolume;
+/*
+ * A Java programm that calculates the volume of cylinder with user provided data.
+ * @AUTHOR - Eganh Ombaka
+ */
+import java.util.Scanner;
+
+public class cylinderVolume {
+
+    private double h;
+    private double r;
+
+    public cylinderVolume(double h, double r) {
+        this.h = h;
+        this.r = r;
+    }
+
+    public double compute() {
+        double output = 3.142 *r *r *h;
+        return output;
+    }
+
+    public void displayOutput() {
+        double answer = compute();
+        System.out.println(String.format("Area = %.2f", answer));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Enter the Radius of the cylinder : ");
+        double r = scanner.nextDouble();
+
+        System.out.print(" Enter the the height of the cylinder : ");
+        double h = scanner.nextDouble();
+
+        cylinderVolume area = new cylinderVolume(h, r);
+        area.displayOutput();
+
+        scanner.close();
+    }
+}
