0f64d4cbc5d22b2fc4ad7e8a0678c683ee44992e
egan-ombaka <eganombaka97@gmail.com>
2025-02-01 16:40:18 +0300

    java

---
+++ fruits/fruitsChoice.java
@@ -0,0 +1,16 @@
+package fruits;
+/*
+ * @Author Eganh Ombaka
+ */
+
+import java.util.Scanner;
+
+public class fruitsChoice {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the fruit of your choice : ");
+
+        scanner.close();
+    }
+}
