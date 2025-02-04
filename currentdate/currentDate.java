25b339986bd0353e26ae9985b955b74e1b501f0c
egan-ombaka <eganombaka97@gmail.com>
2025-02-04 12:44:29 +0300

    A java programm that gives the current Date and time

---
+++ currentdate/currentDate.java
@@ -0,0 +1,14 @@
+package currentdate;
+/*
+ * AUTHOR Egan Ombaka
+ * A java programm that gives the current Date and time
+ */
+import java.util.Date;
+
+public class currentDate {
+    public static void main (String[] args) {
+        Date currentTime = new Date();
+
+        System.out.println(currentTime);
+    }
+}
